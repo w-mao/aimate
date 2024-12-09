d81d3fac557ead0d841a9da242b5cb1e58f11c9b
w-mao <96536008+w-mao@users.noreply.github.com>
2024-12-09 02:06:48 -0800

    Update settings.py
    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'

--- aimate/settings.py
+++ aimate/settings.py
@@ -139,6 +139,8 @@ STATICFILES_FINDERS = [
 
 ]
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
