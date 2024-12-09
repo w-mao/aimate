88d3a3ce1c3ef594cff7726effb816b15c6016e4
w-mao <96536008+w-mao@users.noreply.github.com>
2024-12-09 00:45:42 -0800

    Update urls.py
    from django.conf import settings

--- aimate/urls.py
+++ aimate/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf import settings
 from django.views.generic.base import TemplateView  # new
 
 urlpatterns = [
