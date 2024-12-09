72074c49c35bdb3f3aedd67aae19e7a39b12fa71
w-mao <96536008+w-mao@users.noreply.github.com>
2024-12-09 03:02:31 -0800

    Update settings.py

--- aimate/settings.py
+++ aimate/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = 'media/'
+MEDIA_URL = 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
