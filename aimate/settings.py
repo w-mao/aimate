380f7e688120f0746e7c810015602cac50a90197
w-mao <96536008+w-mao@users.noreply.github.com>
2024-12-09 00:10:34 -0800

    Update settings.py

--- aimate/settings.py
+++ aimate/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+    #'/var/www/static/',
 ]
 
 # Default primary key field type
