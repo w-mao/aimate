4e3c3ab0ada8738d888f04f9a67ad5750e549db5
w-mao <96536008+w-mao@users.noreply.github.com>
2024-12-09 01:47:14 -0800

    Update settings.py
    STATIC_URL = "static/"

--- aimate/settings.py
+++ aimate/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_URL = "/static/"
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
