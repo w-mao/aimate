8d751c0fcab71b562976b5d533de8196bc3a76ad
w-mao <96536008+w-mao@users.noreply.github.com>
2024-12-09 10:51:59 -0800

    Update urls.py
    MEDIA URL

--- aimate/urls.py
+++ aimate/urls.py
@@ -22,4 +22,4 @@ from django.views.generic.base import TemplateView  # new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('members.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
