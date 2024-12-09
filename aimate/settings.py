a55eb29d8a5f2fb78d3f8b56b8f6bea3f4a0f090
w-mao <96536008+w-mao@users.noreply.github.com>
2024-12-09 01:23:25 -0800

    Update settings.py
    if DEBUG:
        STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
    else:
        STATIC_ROOT = os.path.join(BASE_DIR, 'static')

--- aimate/settings.py
+++ aimate/settings.py
@@ -134,8 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    
 #mimetypes.add_type("text/css", ".css", True)
 STATICFILES_DIRS = [
     #BASE_DIR / "static",
